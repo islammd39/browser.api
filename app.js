403f1c5debfc687e49cc35ac11deb18472ff9ab4
islammd39e <islammdrahat308@gmail.comm>
2022-09-05 23:26:17 +0600

    upload file

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const showAlart = ()=>{
 
 
 const confirmm = () =>{
-    const isconfirm =  confirm('this is confirm detels')
+    const isconfirm =  confirm('this is confirm detels orem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum')
      console.log(isconfirm);
      if(isconfirm === true){
         alert( 'yes is true');
